c7c5820d28dc704945e996b1c7affa6996cc7ca3
JakubFojtik <jakub.fojtik@email.cz>
2018-11-10 22:43:01 +0100

    white

--- text-contrast-corrections.user.js
+++ text-contrast-corrections.user.js
@@ -1,3 +1,5 @@
+
+
 // ==UserScript==
 // @name          Text contrast corrections
 // @namespace     https://github.com/JakubFojtik/text-contrast-corrections
@@ -13,18 +15,18 @@
 //Detect background-image or skip color changes if detected. Same for background gradients.
 
 (function () {
-  
-	class Color {
+
+  class Color {
     constructor(colorSpec) {
-      let regex = /[0-9.]+/g;	//allow for missing leading zero, at least FF displays colors like that
+      let regex = /[0-9.]+/g; //allow for missing leading zero, at least FF displays colors like that
       let parts = colorSpec.match(regex);
-      
-      if(parts.length >= 3) {
+
+      if (parts.length >= 3) {
         let parsedParts = [];
         parts.forEach(function (part, idx) {
           parsedParts[idx] = parseFloat(part);
         });
-        if(parsedParts.length < 4) parsedParts[3] = 255;
+        if (parsedParts.length < 4) parsedParts[3] = 255;
         this.parts = parsedParts;
       } else {
         console.log('bad colorspec ' + colorSpec);
@@ -43,8 +45,8 @@
       return 'rgba(' + this.parts.join(', ') + ')';
     }
     brightness() {
-      if(!this.isOpaque()) return 128;	//return 255/2, so that contrast correction still works. Todo compute alpha brightness properly
-      return this.parts.slice(0,4).reduce((a, b) => a + b, 0) / 3;
+      if (!this.isOpaque()) return 128; //return 255/2, so that contrast correction still works. Todo compute alpha brightness properly
+      return this.parts.slice(0, 4).reduce((a, b) => a + b, 0) / 3;
     }
     /*
     asOpaque(brighten) {
@@ -62,7 +64,7 @@
       let fun = brighten ? Math.min : Math.max;
       let limit = brighten ? 255 : 0;
       let op = brighten ? (a, b) => a + b : (a, b) => a - b;
-      for(let i = 0; i < 4; i++) {
+      for (let i = 0; i < 4; i++) {
         this.parts[i] = fun(limit, op(this.parts[i], 80));
       }
     }
@@ -73,7 +75,7 @@
       let saturationLimit = brighten ? 192 : 64;
       //If at least one color part is dark then the color is dark - #f0f is purple.
       //But #ff0 is yellow, todo improve algorithm
-      for(let i = 0; i < 4; i++) {
+      for (let i = 0; i < 4; i++) {
         if (isSaturatedEnough(this.parts[i], saturationLimit)) return true;
       }
       this.changeLum(brighten);
@@ -81,8 +83,8 @@
     }
 
   }
-  
-  
+
+
   function elementsUnder(el) {
     var n, a = [],
       walk = document.createTreeWalker(el, NodeFilter.SHOW_ELEMENT, null, false);
@@ -91,7 +93,7 @@
   }
 
   function computeColor(element, prop) {
-  	var col = new Color(window.getComputedStyle(element).getPropertyValue(prop));
+    var col = new Color(window.getComputedStyle(element).getPropertyValue(prop));
 
     if (!col.isOpaque()) { //Background color can not be computed, if not directly set returns rgba(0,0,0,0)
       let colors = [col];
@@ -99,30 +101,30 @@
       let el = element;
       while (el.parentNode != null) {
         el = el.parentNode;
-      	col = new Color(window.getComputedStyle(el).getPropertyValue(prop)); //Is getComputedStyle inspecting also parent elements for non-computable bgcolor? If yes, optimize?
-        if(!col.isTransparent()) colors.push(col);
+        col = new Color(window.getComputedStyle(el).getPropertyValue(prop)); //Is getComputedStyle inspecting also parent elements for non-computable bgcolor? If yes, optimize?
+        if (!col.isTransparent()) colors.push(col);
         if (col.isOpaque()) { //Only accepts fully opaque color, partialy transparent colors are ignored. Todo compute them into the resulting color.
           bgcolor = col;
           break;
         }
       }
-      if(element.parentNode == null) colors.push(bgcolor);
+      if (element.parentNode == null) colors.push(bgcolor);
       col = bgcolor;
-      
+
       //if(element.tagName.localeCompare('code', 'en', {sensitivity: 'accent'}) == 0) alert(colors.reverse().slice(1));
 
       //Compute all alpha colors with the final opaque color
       //So Blue->10%Red->15%Green should be 85%(90%Blue+10%Red)+15%Green
       //Todo proper alpha blending, this does not seem to give correct results with e.g (1,1,1,0.05)
-      colors.reverse().slice(1).forEach(function(color, idx, arr){
-        let newColPerc = color.alpha()/255;
-        color.parts.slice(0,4).forEach(function (part, idx) {
+      colors.reverse().slice(1).forEach(function (color, idx, arr) {
+        let newColPerc = color.alpha() / 255;
+        color.parts.slice(0, 4).forEach(function (part, idx) {
           let newPart = part * newColPerc;
-          let oldPart = col.parts[idx] * (1-newColPerc);
+          let oldPart = col.parts[idx] * (1 - newColPerc);
           col.parts[idx] = newPart + oldPart;
         });
       });
-      col.parts[3]=255;
+      col.parts[3] = 255;
 
       //Todo create global dictionary of elemt->bgcolor for later lookup. Also assign computed bgcolor to elements between the current and the colored.
       //So that  Blue->transp->transp->transp becomes not only Blue->transp->transp->Blue,
@@ -140,7 +142,7 @@
     }
 
     //if(element.tagName.localeCompare('code', 'en', {sensitivity: 'accent'}) == 0) alert(prop + col + col.isOpaque());
-    
+
     return col;
   }
 
@@ -156,10 +158,10 @@
     //if(isColBrighter) alert('pre'+colParts + ', ' + bgcParts);
     //if(element.tagName.localeCompare('code', 'en', {sensitivity: 'accent'}) == 0)
     //  alert(col+bgcol);
-    if(!col.correct(isColBrighter)) {
+    if (!col.correct(isColBrighter)) {
       element.style.setProperty("color", col.toString(), "important");
     }
-    if(!bgcol.correct(!isColBrighter)) {
+    if (!bgcol.correct(!isColBrighter)) {
       element.style.setProperty("background-color", bgcol.toString(), "important");
     }
     //if(element.tagName.localeCompare('code', 'en', {sensitivity: 'accent'}) == 0)
